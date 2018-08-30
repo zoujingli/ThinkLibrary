c5678b0195088d16d3c1616a39caa33f501fb131
Anyon <zoujingli@qq.com>
2018-08-30 10:44:19 +0800

    [更新]更新bug

--- library/logic/Page.php
+++ library/logic/Page.php
@@ -88,12 +88,22 @@ class Page extends Logic
         }
         // 列表分页及结果集处理
         if ($this->isPage) {
-            $rows = intval($this->request->get('rows', cookie('page-rows')));
-            cookie('page-rows', $rows = $rows >= 10 ? $rows : 20);
-            $page = $this->db->paginate($rows, $this->total, ['query' => $this->request->get()]);
-            $html = "<div class='pagination-trigger nowrap'><span>共 {$page->total()} 条记录，每页显示 {$rows} 条，共 {$page->lastPage()} 页当前显示第 {$page->currentPage()} 页。</span>{$page->render()}</div>";
+            // 分页每页显示记录数
+            $limit = intval($this->request->get('limit', cookie('page-limit')));
+            cookie('page-limit', $limit = $limit >= 10 ? $limit : 20);
+
+            list($rows, $query) = [[], $this->request->get()];
+            $page = $this->db->paginate($limit, $this->total, ['query' => $query]);
+
+            foreach ([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200] as $num) {
+                list($query['limit'], $query['page'], $selected) = [$num, '1', $limit === $num ? 'selected' : ''];
+                $url = url('@admin') . '#' . $this->request->baseUrl() . '?' . http_build_query($query);
+                $rows[] = "<option data-num='{$num}' value='{$url}' {$selected}>{$num}</option>";
+            }
+            $select = "<select onchange='location.href=this.options[this.selectedIndex].value' data-auto-none>" . join('', $rows) . "</select>";
+            $html = "<div class='pagination-trigger nowrap'><span>共 {$page->total()} 条记录，每页显示 {$select} 条，共 {$page->lastPage()} 页当前显示第 {$page->currentPage()} 页。</span>{$page->render()}</div>";
             $this->class->assign('pagehtml', preg_replace('|href="(.*?)"|', 'data-open="$1"', $html));
-            $result = ['page' => ['limit' => intval($rows), 'total' => intval($page->total()), 'pages' => intval($page->lastPage()), 'current' => intval($page->currentPage())], 'list' => $page->items()];
+            $result = ['page' => ['limit' => intval($limit), 'total' => intval($page->total()), 'pages' => intval($page->lastPage()), 'current' => intval($page->currentPage())], 'list' => $page->items()];
         } else {
             $result = ['list' => $this->db->select()];
         }
