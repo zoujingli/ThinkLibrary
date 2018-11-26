cda96876d41b142dcacd173a557bbac5590e126d
Anyon <zoujingli@qq.com>
2018-11-26 11:36:43 +0800

    Update Controller.php

--- library/Controller.php
+++ library/Controller.php
@@ -15,13 +15,23 @@
 namespace library;
 
 use library\tools\Cors;
-use think\db\Query;
 use think\exception\HttpResponseException;
 
 /**
  * 标准控制器基类
+ * --------------------------------
  * Class Controller
  * @package library
+ * --------------------------------
+ * @method logic\Query _query($dbQuery)
+ * @method array _input($data, $rule = [], $info = [])
+ * @method mixed _delete($dbQuery, $pkField = '', $where = [])
+ * @method mixed _save($dbQuery, $data = [], $pkField = '', $where = [])
+ * @method array _page($dbQuery, $isPage = true, $isDisplay = true, $total = false, $limit = 0)
+ * @method mixed _form($dbQuery, $tplFile = '', $pkField = '', $where = [], $data = [])
+ * --------------------------------
+ * @author Anyon <zoujingli@qq.com>
+ * @date 2018/08/10 11:31
  */
 class Controller extends \stdClass
 {
@@ -46,10 +56,16 @@ class Controller extends \stdClass
      * @param string $method 函数名称
      * @param array $arguments 调用参数
      * @return mixed
+     * @throws \ReflectionException
      * @throws \think\Exception
      */
     public function __call($method, $arguments = [])
     {
+        $name = "library\\logic\\" . ucfirst(ltrim($method, '_'));
+        if (class_exists($name)) {
+            $reflect = new \ReflectionClass($name);
+            return $reflect->newInstanceArgs($arguments)->init($this);
+        }
         if (method_exists($this, $method)) {
             return call_user_func_array([$this, $method], $arguments);
         }
@@ -133,86 +149,6 @@ class Controller extends \stdClass
     protected function assign($name, $value = '')
     {
         app('view')->assign($name, $value);
-    }
-
-    /**
-     * 输入指令绑定
-     * @param array $data 验证数据
-     * @param array $rule 验证规则
-     * @param array $info 验证消息
-     * @return array
-     */
-    protected function _input($data, $rule = [], $info = [])
-    {
-        return (new \library\logic\Input($data, $rule, $info))->init($this);
-    }
-
-    /**
-     * 更新指令绑定
-     * @param string|Query $dbQuery
-     * @param string $pkField 指定数据对象主键
-     * @param array $where 额外更新条件
-     * @param array $data 表单扩展数据
-     * @return boolean
-     * @throws \think\Exception
-     * @throws \think\exception\PDOException
-     */
-    protected function _save($dbQuery, $data = [], $pkField = '', $where = [])
-    {
-        return (new \library\logic\Save($dbQuery, $data, $pkField, $where))->init($this);
-
-    }
-
-    /**
-     * 列表指令绑定
-     * @param string $dbQuery 数据库查询对象
-     * @param boolean $isPage 是否启用分页
-     * @param boolean $isDisplay 是否渲染模板
-     * @param boolean $total 集合分页记录数
-     * @param integer $limit 集合每页记录数
-     * @return mixed
-     * @throws \think\Exception
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
-     * @throws \think\exception\PDOException
-     */
-    protected function _page($dbQuery, $isPage = true, $isDisplay = true, $total = false, $limit = 0)
-    {
-        return (new \library\logic\Page($dbQuery, $isPage, $isDisplay, $total, $limit))->init($this);
-    }
-
-    /**
-     * 表单指令绑定
-     * @param string|Query $dbQuery
-     * @param string $tplFile 模板名称
-     * @param string $pkField 指定数据对象主键
-     * @param array $where 额外更新条件
-     * @param array $data 表单扩展数据
-     * @return array|boolean
-     * @throws \think\Exception
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
-     * @throws \think\exception\PDOException
-     */
-    protected function _form($dbQuery, $tplFile = '', $pkField = '', $where = [], $data = [])
-    {
-        return (new \library\logic\Form($dbQuery, $tplFile, $pkField, $where, $data))->init($this);
-    }
-
-    /**
-     * 删除指令绑定
-     * @param string|Query $dbQuery
-     * @param string $pkField 指定数据对象主键
-     * @param array $where 额外更新条件
-     * @return boolean|null
-     * @throws \think\Exception
-     * @throws \think\exception\PDOException
-     */
-    protected function _delete($dbQuery, $pkField = '', $where = [])
-    {
-        return (new \library\logic\Delete($dbQuery, $pkField, $where))->init($this);
     }
 
 }
