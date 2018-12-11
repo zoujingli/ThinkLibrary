fedacba1679666a7b07bf62ebca41bb61d4e8ac6
Anyon <zoujingli@qq.com>
2018-12-11 14:02:27 +0800

    Update Controller.php

--- library/Controller.php
+++ library/Controller.php
@@ -146,8 +146,8 @@ class Controller extends \stdClass
      */
     protected function assign($name, $value = '')
     {
+        if (is_string($name)) $this->{$name} = $value;
         if (is_array($name)) foreach ($name as $k => $v) if (is_string($k)) $this->{$k} = $v;
-        elseif (is_string($name)) $this->{$name} = $value;
         return $this;
     }
 
