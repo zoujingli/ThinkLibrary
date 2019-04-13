6d5c8df53057a082cd090b0857da3097b5cde7e4
Anyon <zoujingli@qq.com>
2019-04-13 12:45:59 +0800

    Update Express.php

--- src/tools/Express.php
+++ src/tools/Express.php
@@ -31,7 +31,7 @@ class Express
     {
         list($microtime, $clientIp, $list) = [time(), request()->ip(), []];
         $options = ['header' => ['Host' => 'www.kuaidi100.com', 'CLIENT-IP' => $clientIp, 'X-FORWARDED-FOR' => $clientIp], 'cookie_file' => env('runtime_path') . 'temp/cookie'];
-        $location = "https://sp0.baidu.com/9_Q4sjW91Qh3otqbppnN2DJv/pae/channel/data/asyncqury?cb=callback&appid=4001&com=debangwuliu&nu={$number}&vcode=&token=&_={$microtime}";
+        $location = "https://sp0.baidu.com/9_Q4sjW91Qh3otqbppnN2DJv/pae/channel/data/asyncqury?cb=callback&appid=4001&com={$code}&nu={$number}&vcode=&token=&_={$microtime}";
         $result = json_decode(str_replace('/**/callback(', '', trim(http_get($location, [], $options), ')')), true);
         if (empty($result['data']['info']['context'])) { // 第一次可能失败，这里尝试第二次查询
             $result = json_decode(str_replace('/**/callback(', '', trim(http_get($location, [], $options), ')')), true);
