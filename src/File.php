24bcdb7c64369f75501241317b8f5c980d074aad
Anyon <zoujingli@qq.com>
2019-08-01 09:56:59 +0800

    Update File.php

--- src/File.php
+++ src/File.php
@@ -209,7 +209,7 @@ class File
 try {
     // 初始化存储
     File::init();
-    // \think\facade\Log::info(__METHOD__ . ' File storage initialization success');
+    // Log::info(__METHOD__ . ' File storage initialization success');
 } catch (\Exception $e) {
     Log::error(__METHOD__ . " File storage initialization exception. [{$e->getMessage()}]");
 }
