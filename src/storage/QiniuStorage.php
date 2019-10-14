96e32da1dba6fe1934955ad67ceb9730441a3891
Anyon <zoujingli@qq.com>
2019-10-14 11:13:05 +0800

    [更新]ComposerUpdate

--- src/storage/QiniuStorage.php
+++ src/storage/QiniuStorage.php
@@ -16,6 +16,7 @@
 namespace library\storage;
 
 use library\Storage;
+use library\tools\Http;
 
 /**
  * Class QiniuStorage
@@ -23,6 +24,11 @@ use library\Storage;
  */
 class QiniuStorage extends Storage
 {
+    private $bucket;
+    private $domain;
+    private $accessKey;
+    private $secretKey;
+
     /**
      * QiniuStorage constructor.
      * @throws \think\Exception
@@ -30,29 +36,57 @@ class QiniuStorage extends Storage
      */
     public function __construct()
     {
+        // 读取配置文件
+        $this->bucket = sysconf('storage_qiniu_bucket');
+        $this->accessKey = sysconf('storage_qiniu_access_key');
+        $this->secretKey = sysconf('storage_qiniu_secret_key');
+        // 计算文件前置链接
         $type = strtolower(sysconf('storage_qiniu_is_https'));
-        $domain = strtolower(sysconf('storage_qiniu_domain'));
-        if ($type === 'auto') $this->root = "//{$domain}/";
-        elseif ($type === 'http') $this->root = "http://{$domain}/";
-        elseif ($type === 'https') $this->root = "https://{$domain}/";
+        $this->domain = strtolower(sysconf('storage_qiniu_domain'));
+        if ($type === 'auto') $this->root = "//{$this->domain}/";
+        elseif ($type === 'http') $this->root = "http://{$this->domain}/";
+        elseif ($type === 'https') $this->root = "https://{$this->domain}/";
         else throw new \think\Exception('未配置七牛云URL域名哦');
     }
 
     /**
+     * 上传文件内容
      * @param string $name
      * @param string $content
      * @param boolean $safe
-     * @throws \think\Exception
-     * @throws \think\exception\PDOException
+     * @return array
      */
     public function set($name, $content, $safe = false)
     {
-        $bucket = sysconf('storage_qiniu_bucket');
-        $policy = [
-            'returnBody' => '{"filename":"$(key)","url":"' . $this->root . '$(key)"}',
-        ];
+        $policy = $this->safeBase64(json_encode([
+            "deadline"   => time() + 3600, "scope" => "{$this->bucket}:{$name}",
+            'returnBody' => json_encode(['filename' => '$(key)', 'url' => "{$this->root}/$(key)"], JSON_UNESCAPED_UNICODE),
+        ]));
+        $token = "{$this->accessKey}:{$this->safeBase64(hash_hmac('sha1', $policy, $this->secretKey, true))}:{$policy}";
+        list($attrs, $frontier) = [[], uniqid()];
+        foreach (['key' => $name, 'token' => $token, 'fileName' => $name] as $k => $v) {
+            $attrs[] = "--{$frontier}";
+            $attrs[] = "Content-Disposition:form-data; name=\"{$k}\"";
+            $attrs[] = "";
+            $attrs[] = $v;
+        }
+        $attrs[] = "--{$frontier}";
+        $attrs[] = "Content-Disposition:form-data; name=\"file\"; filename=\"{$name}\"";
+        $attrs[] = "";
+        $attrs[] = $content;
+        $attrs[] = "--{$frontier}--";
+        $result = Http::post('http://up-z2.qiniup.com/', join("\r\n", $attrs), ['headers' => ["Content-type:multipart/form-data;boundary={$frontier}"]]);
+        return json_decode($result, true);
+    }
 
-        // TODO: Implement put() method.
+    /**
+     * URL安全的Base64编码
+     * @param string $content
+     * @return string
+     */
+    private function safeBase64($content)
+    {
+        return str_replace(['+', '/'], ['-', '_'], base64_encode($content));
     }
 
     public function get($name, $safe = false)
