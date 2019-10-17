4d14b01896fa32fb2cab9ccc8569240161a2219e
Anyon <zoujingli@qq.com>
2019-10-17 14:01:28 +0800

    Update Storage.php

--- src/admin/facade/Storage.php
+++ src/admin/facade/Storage.php
@@ -27,7 +27,7 @@ use think\Facade;
  * @method string url($name, $safe = false) static 获取文件地址
  * @method string set($name, $content, $safe = false) static 文件储存
  * @method string path($name, $safe = false) static 文件存储路径
- * @method boolean del($name, $safe = false) static 删除存储
+ * @method boolean del($name, $safe = false) static 删除存储文件
  * @method boolean has($name, $safe = false) static 检查文件是否存在
  */
 class Storage extends Facade
