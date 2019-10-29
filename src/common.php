1c4fd2978b4154a88317e5c25ffd5f0ea33e18cc
Anyon <zoujingli@qq.com>
2019-10-29 15:32:40 +0800

    Update common.php

--- src/common.php
+++ src/common.php
@@ -21,17 +21,16 @@ if (!function_exists('p')) {
     /**
      * 打印输出数据到文件
      * @param mixed $data 输出的数据
-     * @param boolean $force 强制替换
+     * @param boolean $replace 强制替换
      * @param string|null $file 文件名称
      */
-    function p($data, $force = false, $file = null)
+    function p($data, $replace = false, $file = null)
     {
         if (is_null($file)) $file = env('runtime_path') . date('Ymd') . '.txt';
         $str = (is_string($data) ? $data : (is_array($data) || is_object($data)) ? print_r($data, true) : var_export($data, true)) . PHP_EOL;
-        $force ? file_put_contents($file, $str) : file_put_contents($file, $str, FILE_APPEND);
+        $replace ? file_put_contents($file, $str) : file_put_contents($file, $str, FILE_APPEND);
     }
 }
-
 
 if (!function_exists('systoken')) {
     /**
