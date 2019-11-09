e561d021e18776a9851646236e6af556c77eec6a
Anyon <zoujingli@qq.com>
2019-11-09 16:03:23 +0800

    Update common.php

--- src/common.php
+++ src/common.php
@@ -28,7 +28,7 @@ if (!function_exists('p')) {
      */
     function p($data, $replace = false, $file = null)
     {
-        if (is_null($file)) $file = env('runtime_path') . date('Ymd') . '.txt';
+        if (is_null($file)) $file = app()->getRuntimePath() . date('Ymd') . '.txt';
         $str = (is_string($data) ? $data : (is_array($data) || is_object($data)) ? print_r($data, true) : var_export($data, true)) . PHP_EOL;
         $replace ? file_put_contents($file, $str) : file_put_contents($file, $str, FILE_APPEND);
     }
