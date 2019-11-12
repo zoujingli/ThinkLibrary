69653c3e107f8775c5dc38a18303c6d1011e27bc
Anyon <zoujingli@qq.com>
2019-11-12 11:05:33 +0800

    Update ListenQueue.php

--- src/queue/ListenQueue.php
+++ src/queue/ListenQueue.php
@@ -53,7 +53,7 @@ class ListenQueue extends Command
         while (true) {
             foreach ($this->app->db->name('SystemQueue')->where([['status', '=', '1'], ['exec_time', '<=', time()]])->order('exec_time asc')->select() as $vo) {
                 try {
-                    $this->app->db->name('SystemQueue')->where(['id' => $vo['id']])->update(['status' => '2', 'enter_time' => time(), 'attempts' => $vo['attempts'] + 1]);
+                    $this->app->db->name('SystemQueue')->where(['id' => $vo['id']])->update(['status' => '2', 'enter_time' => time(), 'exec_desc' => '', 'attempts' => $vo['attempts'] + 1]);
                     if ($process->query($command = $process->think("xtask:_work {$vo['id']} -"))) {
                         $output->comment("正在执行 --> [{$vo['id']}] {$vo['title']}");
                     } else {
