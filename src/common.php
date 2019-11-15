7c6f785e12ca67d1153dc84653a2147c8ebec4cc
Anyon <zoujingli@qq.com>
2019-11-15 17:34:51 +0800

    Update common.php

--- src/common.php
+++ src/common.php
@@ -233,21 +233,20 @@ if (!function_exists('emoji_clear')) {
 }
 
 // 注册跨域中间键
-if (PHP_SAPI !== 'cli') {
-    Middleware::add(function (Request $request, \Closure $next, $header = []) {
-        if (($origin = $request->header('origin', '*')) !== '*') {
-            $header['Access-Control-Allow-Origin'] = $origin;
-            $header['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE';
-            $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With';
-            $header['Access-Control-Expose-Headers'] = 'User-Token-Csrf';
-        }
-        if ($request->isOptions()) {
-            return Response::create()->code(204)->header($header);
-        } else {
-            return $next($request)->header($header);
-        }
-    });
-}
+if (PHP_SAPI !== 'cli') Middleware::add(function (Request $request, \Closure $next, $header = []) {
+    if (($origin = $request->header('origin', '*')) !== '*') {
+        $header['Access-Control-Allow-Origin'] = $origin;
+        $header['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,PUT,DELETE';
+        $header['Access-Control-Allow-Headers'] = 'Authorization,Content-Type,If-Match,If-Modified-Since,If-None-Match,If-Unmodified-Since,X-Requested-With';
+        $header['Access-Control-Expose-Headers'] = 'User-Token-Csrf';
+    }
+    if ($request->isOptions()) {
+        return Response::create()->code(204)->header($header);
+    } else {
+        return $next($request)->header($header);
+    }
+});
+
 // 注册系统常用指令
 Console::addDefaultCommands([
     'library\command\Sess',
