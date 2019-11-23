d003ea0488e1e6b96e6a15a98eaec902d60f8885
Anyon <zoujingli@qq.com>
2019-11-23 16:16:58 +0800

    Update SystemService.php

--- src/service/SystemService.php
+++ src/service/SystemService.php
@@ -176,4 +176,24 @@ class SystemService extends Service
         $new ? file_put_contents($file, $str) : file_put_contents($file, $str, FILE_APPEND);
     }
 
+    /**
+     * 是否为本地模式
+     * @return boolean
+     */
+    public function isLocalMode()
+    {
+        $domain = $this->app->request->host(true);
+        return in_array($domain, ['127.0.0.1', 'localhost']);
+    }
+
+    /**
+     * 是否为演示模式
+     * @return boolean
+     */
+    public function isDemoMode()
+    {
+        $domain = $this->app->request->host(true);
+        return is_numeric(stripos($domain, 'thinkadmin.top'));
+    }
+
 }
