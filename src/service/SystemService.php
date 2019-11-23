f4227bd9d2d2d1634bd749cf47642f61a43c507e
Anyon <zoujingli@qq.com>
2019-11-23 16:36:34 +0800

    Update SystemService.php

--- src/service/SystemService.php
+++ src/service/SystemService.php
@@ -178,18 +178,18 @@ class SystemService extends Service
 
     /**
      * 判断运行环境
-     * @param string $type 运行模式（deve|demo|local）
+     * @param string $type 运行模式（dev|demo|local）
      * @return boolean
      */
-    public function isRunMode($type = 'deve')
+    public function checkRunMode($type = 'dev')
     {
         $domain = $this->app->request->host(true);
         $isDemo = is_numeric(stripos($domain, 'thinkadmin.top'));
         $isLocal = in_array($domain, ['127.0.0.1', 'localhost']);
+        if ($type === 'dev') return $isLocal || $isDemo;
         if ($type === 'demo') return $isDemo;
         if ($type === 'local') return $isLocal;
-        if ($type === 'deve') return $isLocal || $isDemo;
-        return false;
+        return true;
     }
 
 }
