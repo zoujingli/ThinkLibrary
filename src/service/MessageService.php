edffe2bab57f971d736ef754c8fc1571139c679a
Anyon <zoujingli@qq.com>
2019-12-24 10:06:51 +0800

    Update MessageService.php

--- src/service/MessageService.php
+++ src/service/MessageService.php
@@ -26,10 +26,10 @@ use think\admin\Service;
  *
  *  CREATE TABLE `system_message_history` (
  *     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
- *     `region` varchar(255) DEFAULT '0' COMMENT '国家编号',
  *     `phone` varchar(100) DEFAULT '' COMMENT '目标手机',
- *     `content` varchar(512) DEFAULT '' COMMENT '短信内容',
+ *     `region` varchar(255) DEFAULT '' COMMENT '国家编号',
  *     `result` varchar(100) DEFAULT '' COMMENT '返回结果',
+ *     `content` varchar(512) DEFAULT '' COMMENT '短信内容',
  *     `create_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
  *     PRIMARY KEY (`id`) USING BTREE,
  *     KEY `idx_system_message_history_phone` (`phone`) USING BTREE,
