f01747b86bc41016fc541feb28359fc2248b846d
Anyon <zoujingli@qq.com>
2020-04-10 10:32:39 +0800

    增加修复指令

--- src/command/database/Repair.php
+++ src/command/database/Repair.php
@@ -13,23 +13,23 @@
 // | github 代码仓库：https://github.com/zoujingli/ThinkLibrary
 // +----------------------------------------------------------------------
 
-namespace think\admin\command;
+namespace think\admin\command\database;
 
 use think\admin\Command;
 use think\console\Input;
 use think\console\Output;
 
 /**
- * 数据库优化指定
- * Class Optimize
- * @package think\admin\command
+ * 数据库修复指令
+ * Class Repair
+ * @package think\admin\command\database
  */
-class Optimize extends Command
+class Repair extends Command
 {
     protected function configure()
     {
         $this->setName('xadmin:optimize');
-        $this->setDescription("Optimize all database tables");
+        $this->setDescription("Attempt to repair all data tables");
     }
 
     /**
@@ -43,18 +43,17 @@ class Optimize extends Command
     protected function execute(Input $input, Output $output)
     {
         $tables = [];
-        $this->setQueueProgress(2, "正在获取需要优化的数据表", 0);
+        $this->setQueueProgress(2, "正在获取需要修复的数据表", 0);
         foreach ($this->app->db->query("show tables") as $item) {
             $tables = array_merge($tables, array_values($item));
         }
         list($total, $used) = [count($tables), 0];
-        $this->setQueueProgress(2, "总共需要优化 {$total} 张数据表", 0);
+        $this->setQueueProgress(2, "总共需要修复 {$total} 张数据表", 0);
         foreach ($tables as $table) {
             $stridx = str_pad(++$used, strlen("{$total}"), ' ', STR_PAD_LEFT) . "/{$total}";
-            $this->setQueueProgress(2, "[{$stridx}] 正在优化数据表 {$table}", $used / $total * 100);
-            $this->app->db->query("OPTIMIZE TABLE `{$table}`");
+            $this->setQueueProgress(2, "[{$stridx}] 正在修复数据表 {$table}", $used / $total * 100);
+            $this->app->db->query("REPAIR TABLE `{$table}`");
         }
-        $this->setQueueMessage(3, '数据库优化完成！');
+        $this->setQueueMessage(3, '数据库修复完成！');
     }
-
 }
