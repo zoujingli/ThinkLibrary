995a999699484fcd84f55c0fd603ae0fe28c7ed9
Anyon <zoujingli@qq.com>
2020-08-12 10:58:09 +0800

    Update Queue.php

--- src/command/Queue.php
+++ src/command/Queue.php
@@ -225,7 +225,7 @@ class Queue extends Command
         if ($this->process->iswin()) {
             $this->setProcessTitle("ThinkAdmin {$this->process->version()} Queue Listen");
         }
-        $this->output->writeln("You can exit with <info>`CTRL-C`</info>");
+        $this->output->writeln("\tYou can exit with <info>`CTRL-C`</info>");
         $this->output->writeln('============== LISTENING ==============');
         while (true) {
             [$start, $where] = [microtime(true), [['status', '=', 1], ['exec_time', '<=', time()]]];
