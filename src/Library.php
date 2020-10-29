0b5a2e260eefb81991b5c45e24ee24c0cda4cec1
Anyon <zoujingli@qq.com>
2020-10-29 09:48:29 +0800

    Update Library.php

--- src/Library.php
+++ src/Library.php
@@ -112,12 +112,12 @@ class Library extends Service
             }, 'route');
         }
         // 动态加入应用初始化系统函数
-        $base = $this->app->getBasePath();
-        foreach (glob("{$base}*/sys.php") as $file) includeFile($file);
+        [$ds, $base] = [DIRECTORY_SEPARATOR, $this->app->getBasePath()];
+        foreach (glob("{$base}*{$ds}sys.php") as $file) includeFile($file);
         // 动态加载插件初始化系统函数
-        $base = "{$this->app->getBasePath()}addons/";
+        $base = "{$this->app->getBasePath()}addons{$ds}";
         if (file_exists($base) && is_dir($base)) {
-            foreach (glob("{$base}*/sys.php") as $file) includeFile($file);
+            foreach (glob("{$base}*{$ds}sys.php") as $file) includeFile($file);
         }
     }
 }
