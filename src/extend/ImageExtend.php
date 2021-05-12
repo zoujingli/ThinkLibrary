db800f1ab27e54dd7ae814ba102780595f533b95
邹景立 <zoujingli@qq.com>
2021-05-12 13:57:15 +0800

    添加图片压缩处理

---
+++ src/extend/ImageExtend.php
@@ -0,0 +1,126 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Library for ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2021 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 仓库地址 ：https://gitee.com/zoujingli/ThinkLibrary
+// | github 仓库地址 ：https://github.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
+
+namespace think\admin\extend;
+
+/**
+ * Image 压缩工具
+ * Class ImageExtend
+ * @package think\admin\extend
+ */
+class ImageExtend
+{
+    private $src;
+    private $image;
+    private $imageinfo;
+    private $percent = 0.5;
+
+    /**
+     * 图片压缩
+     * @param string $src 源图
+     * @param float $percent 压缩比例
+     */
+    public function __construct($src, $percent = 1)
+    {
+        $this->src = $src;
+        $this->percent = $percent;
+    }
+
+
+    /** 高清压缩图片
+     * @param string $saveName 提供图片名（可不带扩展名，用源图扩展名）用于保存。或不提供文件名直接显示
+     */
+    public function compressImg(string $saveName = '')
+    {
+        $this->_openImage();
+        empty($saveName) ? $this->_showImage() : $this->_saveImage($saveName);
+    }
+
+    /**
+     * 内部：打开图片
+     */
+    private function _openImage()
+    {
+        [$width, $height, $type, $attr] = getimagesize($this->src);
+        $this->imageinfo = [
+            'width'  => $width,
+            'height' => $height,
+            'type'   => image_type_to_extension($type, false),
+            'attr'   => $attr,
+        ];
+        $fun = "imagecreatefrom" . $this->imageinfo['type'];
+        $this->image = $fun($this->src);
+        $this->_thumpImage();
+    }
+
+    /**
+     * 内部：操作图片
+     */
+    private function _thumpImage()
+    {
+        $newWidth = $this->imageinfo['width'] * $this->percent;
+        $newHeight = $this->imageinfo['height'] * $this->percent;
+        $image_thump = imagecreatetruecolor($newWidth, $newHeight);
+        //将原图复制带图片载体上面，并且按照一定比例压缩,极大的保持了清晰度
+        imagecopyresampled($image_thump, $this->image, 0, 0, 0, 0, $newWidth, $newHeight, $this->imageinfo['width'], $this->imageinfo['height']);
+        imagedestroy($this->image);
+        $this->image = $image_thump;
+    }
+
+    /**
+     * 输出图片:保存图片则用saveImage()
+     */
+    private function _showImage()
+    {
+        header('Content-Type: image/' . $this->imageinfo['type']);
+        $funcs = "image" . $this->imageinfo['type'];
+        $funcs($this->image);
+    }
+
+    /**
+     * 保存图片到硬盘：
+     * @param string $dstImgName 1、可指定字符串不带后缀的名称，使用源图扩展名 。2、直接指定目标图片名带扩展名。
+     */
+    private function _saveImage(string $dstImgName): void
+    {
+        if (empty($dstImgName)) return;
+        $allowImgs = ['.jpg', '.jpeg', '.png', '.bmp', '.wbmp', '.gif'];   //如果目标图片名有后缀就用目标图片扩展名 后缀，如果没有，则用源图的扩展名
+        $dstExt = strrchr($dstImgName, ".");
+        $sourseExt = strrchr($this->src, ".");
+        if (!empty($dstExt)) $dstExt = strtolower($dstExt);
+        if (!empty($sourseExt)) $sourseExt = strtolower($sourseExt);
+
+        //有指定目标名扩展名
+        if (!empty($dstExt) && in_array($dstExt, $allowImgs)) {
+            $dstName = $dstImgName;
+        } elseif (!empty($sourseExt) && in_array($sourseExt, $allowImgs)) {
+            $dstName = $dstImgName . $sourseExt;
+        } else {
+            $dstName = $dstImgName . $this->imageinfo['type'];
+        }
+        $funcs = "image" . $this->imageinfo['type'];
+        $funcs($this->image, $dstName);
+    }
+
+    /**
+     * 销毁图片
+     */
+    public function __destruct()
+    {
+        imagedestroy($this->image);
+    }
+}
