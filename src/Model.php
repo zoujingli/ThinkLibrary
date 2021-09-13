42d59350b1c499113d3015b64ed75dfd3e2911f2
邹景立 <zoujingli@qq.com>
2021-09-13 15:30:11 +0800

    Update Model.php

--- src/Model.php
+++ src/Model.php
@@ -15,8 +15,10 @@
 
 namespace think\admin;
 
+use think\admin\helper\DeleteHelper;
+use think\admin\helper\FormHelper;
 use think\admin\helper\QueryHelper;
-use think\db\exception\DbException;
+use think\admin\helper\SaveHelper;
 
 /**
  * 基础模型类
@@ -50,14 +52,55 @@ abstract class Model extends \think\Model
     }
 
     /**
+     * 快捷表单逻辑器
+     * @param string $template 模板名称
+     * @param string $field 指定数据对象主键
+     * @param array $where 额外更新条件
+     * @param array $data 表单扩展数据
+     * @return array|bool
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public static function mForm(string $template = '', string $field = '', array $where = [], array $data = [])
+    {
+        return FormHelper::instance()->init(static::mk(), $template, $field, $where, $data);
+    }
+
+    /**
+     * 快捷更新逻辑器
+     * @param array $data 表单扩展数据
+     * @param string $field 数据对象主键
+     * @param array $where 额外更新条件
+     * @return bool
+     * @throws \think\db\exception\DbException
+     */
+    public static function mSave(array $data = [], string $field = '', array $where = []): bool
+    {
+        return SaveHelper::instance()->init(static::mk(), $data, $field, $where);
+    }
+
+    /**
      * 快捷查询逻辑器
      * @param array|string|null $input
      * @return QueryHelper
-     * @throws DbException
+     * @throws \think\db\exception\DbException
      */
     public static function mQuery($input = null): QueryHelper
     {
         return QueryHelper::instance()->init(static::mk(), $input);
+    }
+
+    /**
+     * 快捷删除逻辑器
+     * @param string $field 数据对象主键
+     * @param array $where 额外更新条件
+     * @return bool|null
+     * @throws \think\db\exception\DbException
+     */
+    public static function mDelete(string $field = '', array $where = []): ?bool
+    {
+        return DeleteHelper::instance()->init(static::mk(), $field, $where);
     }
 
     /**
