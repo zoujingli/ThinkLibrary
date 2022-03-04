c20912dc07da433ba4e55f756cd22169bbae8f64
Anyon <zoujingli@qq.com>
2022-03-04 15:45:18 +0800

    优化数据树生成

--- src/extend/DataExtend.php
+++ src/extend/DataExtend.php
@@ -27,20 +27,16 @@ class DataExtend
 
     /**
      * 一维数组生成数据树
-     * @param array $list 待处理数据
+     * @param array $its 待处理数据
      * @param string $cid 自己的主键
      * @param string $pid 上级的主键
      * @param string $sub 子数组名称
      * @return array
      */
-    public static function arr2tree(array $list, string $cid = 'id', string $pid = 'pid', string $sub = 'sub'): array
+    public static function arr2tree(array $its, string $cid = 'id', string $pid = 'pid', string $sub = 'sub'): array
     {
-        [$tree, $temp] = [[], array_combine(array_column($list, $cid), array_values($list))];
-        foreach ($list as $vo) if (isset($vo[$pid]) && isset($temp[$vo[$pid]])) {
-            $temp[$vo[$pid]][$sub][] = &$temp[$vo[$cid]];
-        } else {
-            $tree[] = &$temp[$vo[$cid]];
-        }
+        [$tree, $its] = [[], array_column($its, null, $cid)];
+        foreach ($its as $it) isset($its[$it[$pid]]) ? $its[$it[$pid]][$sub][] = &$its[$it[$cid]] : $tree[] = &$its[$it[$cid]];
         return $tree;
     }
 
@@ -49,27 +45,30 @@ class DataExtend
      * @param array $list 待处理数据
      * @param string $cid 自己的主键
      * @param string $pid 上级的主键
-     * @param string $cpath 当前 PATH
-     * @param string $ppath 上级 PATH
+     * @param string $path 当前 PATH
      * @return array
      */
-    public static function arr2table(array $list, string $cid = 'id', string $pid = 'pid', string $cpath = 'path', string $ppath = ''): array
+    public static function arr2table(array $list, string $cid = 'id', string $pid = 'pid', string $path = 'path'): array
     {
-        $tree = [];
-        foreach (static::arr2tree($list, $cid, $pid) as $attr) {
-            $attr[$cpath] = "{$ppath}-{$attr[$cid]}";
-            $attr['sub'] = $attr['sub'] ?? [];
-            $attr['spc'] = count($attr['sub']);
-            $attr['spt'] = substr_count($ppath, '-');
-            $attr['spl'] = str_repeat('ㅤ├ㅤ', $attr['spt']);
-            $sub = $attr['sub'];
-            unset($attr['sub']);
-            $tree[] = $attr;
-            if (!empty($sub)) {
-                $tree = array_merge($tree, static::arr2table($sub, $cid, $pid, $cpath, $attr[$cpath]));
+        $call = function (array $list, callable $call, array &$data = [], string $parent = '') use ($cid, $pid, $path) {
+            foreach ($list as $item) {
+                $list = $item['sub'] ?? [];
+                unset($item['sub']);
+                $item[$path] = "{$parent}-{$item[$cid]}";
+                $item['spc'] = count($list);
+                $item['spt'] = substr_count($parent, '-');
+                $item['spl'] = str_repeat('├ㅤ', $item['spt']);
+                $item['sps'] = ",{$item[$cid]},";
+                array_walk_recursive($list, function ($val, $key) use ($cid, &$item) {
+                    if ($key === $cid) $item['sps'] .= "{$val},";
+                });
+                $data[] = $item;
+                if (empty($list)) continue;
+                $call($list, $call, $data, $item[$path]);
             }
-        }
-        return $tree;
+            return $data;
+        };
+        return $call(static::arr2tree($list, $cid, $pid), $call);
     }
 
     /**
