d565f55d3df4dccf4d5835f36520d78aaee40aaa
Anyon <zoujingli@qq.com>
2022-04-21 17:35:19 +0800

    Update AdminService.php

--- src/service/AdminService.php
+++ src/service/AdminService.php
@@ -100,9 +100,8 @@ class AdminService extends Service
      */
     public function setUserData(array $data, bool $replace = false)
     {
-        $keys = "UserData_{$this->getUserId()}";
         $data = $replace ? $data : array_merge_recursive($this->getUserData(), $data);
-        return SystemService::instance()->setData($keys, $data);
+        return SystemService::instance()->setData("UserData_{$this->getUserId()}", $data);
     }
 
     /**
