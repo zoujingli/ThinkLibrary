bca9355c038be698664848c2097c6a628bf5c0eb
Anyon <zoujingli@qq.com>
2022-06-17 15:05:22 +0800

    增加文件管理模型

---
+++ src/model/SystemFile.php
@@ -0,0 +1,51 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2022 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://gitee.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/ThinkLibrary
+// | github 代码仓库：https://github.com/zoujingli/ThinkLibrary
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
+
+namespace think\admin\model;
+
+use think\admin\Model;
+
+/**
+ * 文件管理系统
+ * Class SystemFile
+ * @package think\admin\model
+ */
+class SystemFile extends Model
+{
+    protected $createTime = 'create_at';
+    protected $updateTime = 'update_at';
+
+    /**
+     * 格式化创建时间
+     * @param mixed $value
+     * @return string
+     */
+    public function getCreateAtAttr($value): string
+    {
+        return format_datetime($value);
+    }
+
+    /**
+     * 格式化更新时间
+     * @param mixed $value
+     * @return string
+     */
+    public function getUpdateAtAttr($value): string
+    {
+        return format_datetime($value);
+    }
+}
