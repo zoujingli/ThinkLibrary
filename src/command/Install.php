0dfd751d25a055099d1edaa695e550d95334581e
Anyon <zoujingli@qq.com>
2022-06-24 14:09:16 +0800

    Update Install.php

--- src/command/Install.php
+++ src/command/Install.php
@@ -19,6 +19,7 @@ namespace think\admin\command;
 
 use think\admin\Command;
 use think\admin\service\ModuleService;
+use think\admin\service\SystemService;
 use think\console\Input;
 use think\console\input\Argument;
 use think\console\Output;
@@ -111,11 +112,11 @@ class Install extends Command
                 $this->rules = array_merge($this->rules, $bind['rules']);
                 $this->ignore = array_merge($this->ignore, $bind['ignore']);
             }
-            $this->copyFile('static') && $this->installFile();
+            $this->copyFileAndTable('static') && $this->installFile();
         } elseif (isset($this->bind[$this->name])) {
             $this->rules = $this->bind[$this->name]['rules'] ?? [];
             $this->ignore = $this->bind[$this->name]['ignore'] ?? [];
-            $this->copyFile($this->name) && $this->installFile();
+            $this->copyFileAndTable($this->name) && $this->installFile();
         } else {
             $this->output->writeln("The specified module {$this->name} is not configured with install rules");
         }
@@ -150,20 +151,11 @@ class Install extends Command
     }
 
     /**
-     * 安装数据库
-     * @return boolean
-     */
-    protected function installData(): bool
-    {
-        return true;
-    }
-
-    /**
      * 初始化安装文件
      * @param string $type
      * @return boolean
      */
-    private function copyFile(string $type): bool
+    private function copyFileAndTable(string $type): bool
     {
         if ($type === 'static') {
             $frdir = dirname(__DIR__) . "/service/bin/{$type}/";
@@ -175,6 +167,51 @@ class Install extends Command
                 }
             }
         }
+        // 创建系统文件数据表
+        if ($type === 'admin') {
+            $this->createSystemFileTable();
+        }
         return true;
+    }
+
+    /**
+     * 创建系统文件表
+     * @return void
+     */
+    private function createSystemFileTable()
+    {
+        [$tables] = SystemService::getTables();
+        $config = $this->app->db->connect()->getConfig();
+        [$type, $prefix] = [$config['type'] ?? '', $config['prefix'] ?? ''];
+        if ($type === 'mysql' && !in_array($table = "{$prefix}system_file", $tables)) {
+            $this->app->db->connect()->query(<<<SQL
+CREATE TABLE {$table} (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NULL DEFAULT '' COMMENT '上传类型',
+  `hash` varchar(32) NULL DEFAULT '' COMMENT '文件哈希',
+  `name` varchar(200) NULL DEFAULT '' COMMENT '文件名称',
+  `xext` varchar(100) NULL DEFAULT '' COMMENT '文件后缀',
+  `xurl` varchar(500) NULL DEFAULT '' COMMENT '访问链接',
+  `xkey` varchar(500) NULL DEFAULT '' COMMENT '文件路径',
+  `mime` varchar(100) NULL DEFAULT '' COMMENT '文件类型',
+  `size` bigint(20) NULL DEFAULT 0 COMMENT '文件大小',
+  `uuid` bigint(20) NULL DEFAULT 0 COMMENT '用户编号',
+  `isfast` tinyint(1) NULL DEFAULT 0 COMMENT '是否秒传',
+  `issafe` tinyint(1) NULL DEFAULT 0 COMMENT '安全模式',
+  `status` tinyint(1) NULL DEFAULT 1 COMMENT '上传状态(1悬空,2落地)',
+  `create_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_system_file_type`(`type`) USING BTREE,
+  INDEX `idx_system_file_hash`(`hash`) USING BTREE,
+  INDEX `idx_system_file_uuid`(`uuid`) USING BTREE,
+  INDEX `idx_system_file_xext`(`xext`) USING BTREE,
+  INDEX `idx_system_file_status`(`status`) USING BTREE,
+  INDEX `idx_system_file_issafe`(`issafe`) USING BTREE,
+  INDEX `idx_system_file_isfast`(`isfast`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 COMMENT='系统-文件' ROW_FORMAT=COMPACT;
+SQL
+            );
+        }
     }
 }
