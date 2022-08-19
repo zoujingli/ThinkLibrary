f1e2684c856990f9789fc513a8fd413aabc2e3b4
邹景立 <zoujingli@qq.com>
2022-08-19 12:03:00 +0800

    Update common.php

--- src/common.php
+++ src/common.php
@@ -352,8 +352,9 @@ if (!function_exists('format_datetime')) {
      */
     function format_datetime($datetime, string $format = 'Y年m月d日 H:i:s'): string
     {
-        if (empty($datetime)) return '-';
-        if (is_numeric($datetime)) {
+        if (empty($datetime)) {
+            return '-';
+        } elseif (is_numeric($datetime)) {
             return date($format, intval($datetime));
         } else {
             return date($format, strtotime($datetime));
