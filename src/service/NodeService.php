1b79d9b85d47798968a008558645f4decdc46d98
邹景立 <zoujingli@qq.com>
2022-11-28 09:10:15 +0800

    Update NodeService.php

--- src/service/NodeService.php
+++ src/service/NodeService.php
@@ -119,8 +119,7 @@ class NodeService extends Service
         /*! 排除内置方法，禁止访问内置方法 */
         $ignores = get_class_methods('\think\admin\Controller');
         /*! 扫描所有代码控制器节点，更新节点缓存 */
-        foreach (ToolsExtend::scanDirectory(Library::$sapp->getBasePath(), 'php') as $file) {
-            $name = substr($file, strlen(strtr(Library::$sapp->getRootPath(), '\\', '/')) - 1);
+        foreach (ToolsExtend::scanDirectory(Library::$sapp->getBasePath(), 'php') as $name) {
             if (preg_match("|^([\w/]+)/(\w+)/controller/(.+)\.php$|i", $name, $matches)) {
                 [, $appSpace, $appName, $className] = $matches;
                 static::_parseClass($appSpace, $appName, $className, $ignores, $data);
@@ -130,8 +129,7 @@ class NodeService extends Service
         $defSpace = Library::$sapp->config->get('app.app_namespace') ?: 'app';
         foreach (Library::$sapp->config->get('app.addons', []) as $appName => $appPath) {
             [$appPath, $appSpace] = explode('@', "{$appPath}@");
-            foreach (ToolsExtend::scanDirectory($appPath, 'php') as $file) {
-                $filename = substr($file, strlen(strtr($appPath, '\\', '/')) - 1);
+            foreach (ToolsExtend::scanDirectory($appPath, 'php') as $filename) {
                 if (preg_match("|^.*?/controller/(.+)\.php$|i", $filename, $matches)) {
                     static::_parseClass($appSpace ?: $defSpace, $appName, $matches[1], $ignores, $data);
                 }
