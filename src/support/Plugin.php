386b96ad291118304f1966a563c306eb5ed8fe3a
邹景立 <zoujingli@qq.com>
2022-11-30 16:55:53 +0800

    Update Plugin.php

--- src/support/Plugin.php
+++ src/support/Plugin.php
@@ -3,10 +3,12 @@
 namespace think\admin\support;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
+use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PluginInterface;
 
-class Plugin implements PluginInterface
+class Plugin implements PluginInterface, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -19,6 +21,21 @@ class Plugin implements PluginInterface
     }
 
     public function uninstall(Composer $composer, IOInterface $io)
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        echo __METHOD__ . PHP_EOL;
+        return [
+            PluginEvents::POST_FILE_DOWNLOAD => [
+                ['onFileDonwload', 0],
+            ],
+        ];
+    }
+
+    public static function onFileDonwload()
     {
         echo __METHOD__ . PHP_EOL;
     }
