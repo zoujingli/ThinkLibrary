15611bf82e1383bc00c245cc5d60509cf126b308
邹景立 <zoujingli@qq.com>
2023-07-21 02:03:13 +0800

    修改异常处理

--- src/common.php
+++ src/common.php
@@ -412,11 +412,12 @@ if (!function_exists('trace_file')) {
         if (!file_exists($path)) mkdir($path, 0777, true);
         $name = substr($exception->getFile(), strlen(syspath()));
         $file = $path . DIRECTORY_SEPARATOR . date('Ymd_His_') . strtr($name, ['/' => '.', '\\' => '.']);
+        $json = json_encode($exception instanceof \think\admin\Exception ? $exception->getData() : [], 64 | 128 | 256);
         $class = get_class($exception);
         return false !== file_put_contents($file,
                 "[CODE] {$exception->getCode()}" . PHP_EOL .
                 "[INFO] {$exception->getMessage()}" . PHP_EOL .
-                ($exception instanceof \think\admin\Exception ? "[DATA] {$exception->getData()}" . PHP_EOL : '') .
+                ($exception instanceof \think\admin\Exception ? "[DATA] {$json}" . PHP_EOL : '') .
                 "[FILE] {$class} in {$name} line {$exception->getLine()}" . PHP_EOL .
                 "[TIME] " . date('Y-m-d H:i:s') . PHP_EOL . PHP_EOL .
                 '[TRACE]' . PHP_EOL . $exception->getTraceAsString()
